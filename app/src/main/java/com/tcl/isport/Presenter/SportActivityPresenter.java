3139129afd61f84320b5910d15dc7b913c7119f1
lishui.lin <lishui.lin@tcl.com>
2017-09-23 09:36:36 +0800

    lishui.lin

--- app/src/main/java/com/tcl/isport/Presenter/SportActivityPresenter.java
+++ app/src/main/java/com/tcl/isport/Presenter/SportActivityPresenter.java
@@ -29,7 +29,7 @@ public class SportActivityPresenter {
         else if(view.getClass().getName().equals(RideActivity.class.getName())){
             iSportModel=new RideModel();
         }
-        iSportActivity.setDistance(iSportModel.getDistance());
+       
     }
 
 }
